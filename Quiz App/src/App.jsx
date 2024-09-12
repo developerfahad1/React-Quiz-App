870fa30fcbc24bab5e0a1377cafe6071ac5f7632
developerfahad1 <fahadgraphic.pk>
2024-09-12 10:23:32 +0500

    quiz app

--- Quiz App/src/App.jsx
+++ Quiz App/src/App.jsx
@@ -1,25 +1,141 @@
-import React, { useState } from 'react'
-import Quiz from './components/Quiz'
-import { Data } from './components/Data'
+import axios from "axios"
+import { useEffect, useRef } from "react"
+import { useState } from "react"
 
-const App = () => {
-  const [data, setData] = useState(Data)
-  console.log(Data);
+
+function App(){
+  const [render,setRender] = useState([])
+  const [questionState,setQuestionState] = useState(0)
+  const [score,setScore] = useState(0)
+  const [WrongScore,setWrongScore] = useState(0)
+  const [quizComplete, setQuizComplete] = useState(false)
+  const checkedInput = useRef([])
+
+  try {
+    useEffect(()=>{
+      axios('https://the-trivia-api.com/v2/questions')
+      .then((res)=>{
+        console.log(res.data)
+        setRender(shuffleArray(res.data))
+      })
+      .catch((err)=>{
+        console.log(err);
+        
+      })
+    },[])
+  } catch (error) {
+    console.log(error);
+  }
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    
+    return array;
+  }
+
+
+  function NextQuetsion(){
+    
+    console.log(render.length);
+    
+    const checkedButton = checkedInput.current.find(input => input.checked);
+
+    if(!checkedButton){
+      alert('Please select an option');
+      return; 
+    }
+
+    
+      const selectedValue = checkedButton.value;
+      console.log("Selected answer:", selectedValue);
+
   
-  return (
-    <div className='container'>
-      <div className='quiz'>
-        <div>
-          <h1>{Data[0].q}</h1>
-        </div>
-        <div className='option'>
-          <input type='radio' />
-          <p> {data[0].a}</p>
-         </div>
+      if(selectedValue === render[questionState].correctAnswer){
+        setScore(score + 1)
+        console.log(score);
+      }else{
+        setWrongScore(WrongScore + 1)
+        console.log(WrongScore);
+      }
+      
+      
+  
+    const next = questionState + 1
+    if(next >= render.length - 1){
+      setQuizComplete(true);
+      alert(`Quiz Completed.`);
+      alert(` Your score is ${score}/9`)
+     
+      setRender(shuffleArray(render))
+    }else{
+      setQuestionState(next);
+    }
 
-      </div>
-    </div>
-  )
+
+
+    checkedInput.current.forEach(input=>{
+      if(input){
+        input.checked = false;
+      }
+    })
+  }
+
+  
+  
+  //return JSX
+  return (
+    <>
+    {render.length > 0 ? (
+        <div>
+          {!quizComplete ? (
+            <>
+              <div className="container">
+              <h1 className="quiz">Quiz App</h1>
+              <h2>Q{questionState + 1} : {render[questionState].question.text}</h2>
+              <hr />
+              <div className="main">
+              <ol>
+                {shuffleArray([...render[questionState].incorrectAnswers, render[questionState].correctAnswer])
+                  .map((item, index) => (
+                      <li key={index} className="li">
+                        <input 
+                          id={item}
+                          name="quiz"
+                          type="radio"
+                          value={item}
+                          ref={e => checkedInput.current[index] = e}
+                        />
+                        <label className="li-label" htmlFor={item}>{item}</label>
+                      </li>
+                  ))}
+              </ol>
+              </div>
+              <button className="btn" onClick={NextQuetsion}>Next</button>
+              </div>
+            </>
+          ) : (
+            <div className="quiz-complete">
+              <h1>Quiz Complete</h1>
+              <h3>{`Final Score: ${score}/9`}</h3>
+              <div>
+                <h3>{`Correct Answers: ${score}`}</h3>
+                <h3>{`Wrong Answers: ${WrongScore}`}</h3>
+                <h1>Total: 9</h1>
+                <button className="btn-2" onClick={() => window.location.reload()}>Again</button>
+              </div>
+            </div>
+          )}
+        </div>
+      ) : (
+         <>
+                 <h1 className="loading">Loading...</h1>
+         </>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
